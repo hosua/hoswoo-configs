90da6a003432acd9698b6e7e13228e36d7439196
hosua <hosua.o@gmail.com>
2021-10-29 19:14:30 -0400

    added tmux config

--- dwm-hoswoo/pkg/dwm-distrotube-git/opt/dwm-distrotube-git/config.h
+++ dwm-hoswoo/pkg/dwm-distrotube-git/opt/dwm-distrotube-git/config.h
@@ -8,7 +8,7 @@
 /* appearance */
 static const unsigned int borderpx = 2;   /* border pixel of windows */
 static const unsigned int snap     = 32;  /* snap pixel */
-static const unsigned int gappx    = 4;   /* pixel gap between clients */
+static const unsigned int gappx    = 6;   /* pixel gap between clients */
 static const int showbar           = 1;   /* 0 means no bar */
 static const int topbar            = 1;   /* 0 means bottom bar */
 static const int horizpadbar       = 6;   /* horizontal padding for statusbar */
@@ -18,28 +18,22 @@ static const int vertpadbar        = 7;   /* vertical padding for statusbar */
  * Arch repos and is listed as a dependency for this build. JoyPixels is also
  * a hard dependency and makes colored fonts and emojis possible.
  */
-static const char *fonts[]     = {"Mononoki Nerd Font:size=9:antialias=true:autohint=true",
+static const char *fonts[]     = {"SauceCodePro Nerd Font Mono:weight=bold:size=9:antialias=true:hinting=true",
+	                              "Mononoki:size=9:antialias=true:autohint=true",
                                   "Hack:size=8:antialias=true:autohint=true",
                                   "JoyPixels:size=10:antialias=true:autohint=true"
 						     	};
-
-// Original
-// static const char col_1[]  = "#282c34"; /* background color of bar */
-// static const char col_2[]  = "#282c34"; /* border color unfocused windows */
-// static const char col_3[]  = "#d7d7d7";
-// static const char col_4[]  = "#924441"; /* border color focused windows and tags */
-static const char col_1[]  = "#282a36"; /* background color of bar */
-static const char col_2[]  = "#282a36"; /* border color unfocused windows */
-static const char col_3[]  = "#f8f8f2";
-static const char col_4[]  = "#6A39C5"; /* border color focused windows and tags */
-
+static const char col_1[]  = "#282c34"; /* background color of bar */
+static const char col_2[]  = "#282c34"; /* border color unfocused windows */
+static const char col_3[]  = "#d7d7d7";
+static const char col_4[]  = "#924441"; /* border color focused windows and tags */
 /* bar opacity 
  * 0xff is no transparency.
  * 0xee adds wee bit of transparency.
  * 0xdd adds adds a bit more transparency.
  * Play with the value to get desired transparency.
  */
-static const unsigned int baralpha    = 0x99; 
+static const unsigned int baralpha    = 0xff; 
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]        = {
 	/*               fg         bg         border   */
@@ -53,8 +47,9 @@ static const unsigned int alphas[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; 
-/*static const char *tags[] = { "", "", "", "", "", "", "", "", "" };*/
+/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
+/* static const char *tags[] = { "", "", "", "", "", "", "", "", "" }; */
+static const char *tags[] = { "dev", "www", "sys", "doc", "vbox", "chat", "mus", "vid", "gfx" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -105,11 +100,6 @@ static const char *termcmd[]     = { "st", NULL };
 /* An alternative way to launch st along with the fish shell */
 /* static const char *termcmd[]     = { "st", "-e fish", NULL }; */
 static const char *tabtermcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
-static const char *startalsa[]  = {"st", "alsamixer", NULL };
-static const char *screenshot[] = {"scrot", "/home/hoswoo/Pictures/Screenshots/.jpg", "-s", NULL };
-static const char *browsermenu[] = { "/home/hoswoo/Desktop/Scripts/browsermenu.sh", NULL };
-static const char *powermenu[] = { "/home/hoswoo/Desktop/Scripts/powermenu.sh", NULL };
-// static const char *gamemenu[] = { "/home/hoswoo/Desktop/Scripts/gamemenu.sh", NULL };
 
 static Key keys[] = {
 	/* modifier             chain key  key        function        argument */
@@ -152,14 +142,28 @@ static Key keys[] = {
 	
     /* Keybindings for programs using the format SUPER + ALT + "key" */
 	{ MODKEY|Mod1Mask,      -1,        XK_s,      spawn,          CMD("tabbed -r 2 surf -pe x '.surf/html/homepage.html'") },
+	{ MODKEY|Mod1Mask,      -1,        XK_b,      spawn,          CMD("brave") },
 	{ MODKEY|Mod1Mask,      -1,        XK_m,      spawn,          CMD("mailspring") },
-	{ MODKEY|Mod1Mask,      -1,        XK_f,      spawn,          CMD("pcmanfm") },	
-	{ MODKEY|Mod1Mask,      -1,        XK_n,      spawn,          CMD("nm-connection-editor") },
-	{ MODKEY|Mod1Mask,	-1,		XK_v,	spawn,		{.v = startalsa } },
-	{ MODKEY|Mod1Mask,	-1,		XK_Print,	spawn,	{.v = screenshot } },
-	{ MODKEY|Mod1Mask,	-1,		XK_c,		spawn,	CMD("qalculate-gtk") },
-	{ MODKEY|Mod1Mask,      -1,        XK_b,      spawn,          {.v = browsermenu } },
-	{ MODKEY|Mod1Mask,	-1,		XK_p,	spawn,		{.v = powermenu } },
+	{ MODKEY|Mod1Mask,      -1,        XK_f,      spawn,          CMD("pcmanfm") },
+	
+    /* Dmenu scripts launched with emacs-style keychords SUPER + p followed by "key" */
+	{ MODKEY,               XK_p,      XK_e,      spawn,          CMD("./dmscripts/dm-confedit") },
+	{ MODKEY,               XK_p,      XK_i,      spawn,          CMD("./dmscripts/dm-maim") },
+	{ MODKEY,               XK_p,      XK_k,      spawn,          CMD("./dmscripts/dm-kill") },
+	{ MODKEY,               XK_p,      XK_l,      spawn,          CMD("./dmscripts/dm-logout") },
+	{ MODKEY,               XK_p,      XK_m,      spawn,          CMD("./dmscripts/dm-man") },
+	{ MODKEY,               XK_p,      XK_r,      spawn,          CMD("./dmscripts/dm-reddit") },
+	{ MODKEY,               XK_p,      XK_s,      spawn,          CMD("./dmscripts/dm-websearch") },
+	{ MODKEY,               XK_p,      XK_p,      spawn,          CMD("passmenu") },
+    
+    /* Doom emacs keybindings use the keychord CTRL + e followed by "key" */
+	{ ControlMask,          XK_e,      XK_e,      spawn,          CMD("emacsclient -c -a 'emacs'") },
+	{ ControlMask,          XK_e,      XK_b,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(ibuffer)'") },
+	{ ControlMask,          XK_e,      XK_d,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(dired nil)'") },
+	{ ControlMask,          XK_e,      XK_m,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(mu4e)'") },
+	{ ControlMask,          XK_e,      XK_n,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(elfeed)'") },
+	{ ControlMask,          XK_e,      XK_s,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(eshell)'") },
+	{ ControlMask,          XK_e,      XK_v,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(+vterm/here nil)'") },
 
 	TAGKEYS(                -1,        XK_1,                      0)
 	TAGKEYS(                -1,        XK_2,                      1)
