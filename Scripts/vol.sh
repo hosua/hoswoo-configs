9d5623e17bc74d02180040d50b75a82b9a0fdd7d
hosua <hosua.o@gmail.com>
2021-12-06 00:53:11 -0500

    volfix

--- Scripts/vol.sh
+++ Scripts/vol.sh
@@ -1,4 +1,7 @@
-output=$(amixer | grep "Left: Playback"| awk 'NR==1')
+#output=$(amixer | grep "Left: Playback"| awk 'NR==1')
+playback="Mono: Playback"
+
+output=$(amixer | grep "$playback" | awk 'NR==1')
 # Search the line for on or off
 if [[ "$output" == *"on"* ]]; then
 	onoff='on'
@@ -9,12 +12,12 @@ fi
 if [[ $onoff == 'on' ]]
 then 
 	#echo $(amixer sget Master | grep "Mono:" | awk '{print "🔊 "$4}' | sed 's/[][]//g')
-	echo $(amixer | grep "Left: Playback" | awk '{print "🔊 "$5}' | sed 's/[][]//g')
+	echo $(amixer | grep "$playback"  | awk '{print "🔊 "$4}' | sed 's/[][]//g')
 fi
 
 if [[ $onoff == 'off' ]];
 then
 	#echo $(amixer sget Master | grep "Mono:" | awk '{print "🔇 "$4}' | sed 's/[][]//g')
-	echo $(amixer | grep "Left: Playback" | awk '{print "🔇 "$5}' | sed 's/[][]//g')
+	echo $(amixer | grep "$playback" | awk '{print "🔇 "$4}' | sed 's/[][]//g')
 fi
 
