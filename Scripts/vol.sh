8996c3b5b0e7cff4cb86f26abcc84cf5d3699a1a
hosua <hosua.o@gmail.com>
2021-12-06 00:46:52 -0500

    vol

--- Scripts/vol.sh
+++ Scripts/vol.sh
@@ -1,4 +1,11 @@
-onoff=$(amixer | grep "Left: Playback"| awk 'NR==1{print $6}' | sed 's/[][]//g')
+output=$(amixer | grep "Left: Playback"| awk 'NR==1')
+# Search the line for on or off
+if [[ "$output" == *"on"* ]]; then
+	onoff='on'
+elif [[ "$output" == *"off"* ]]; then
+	onoff='off'
+fi
+
 if [[ $onoff == 'on' ]]
 then 
 	#echo $(amixer sget Master | grep "Mono:" | awk '{print "🔊 "$4}' | sed 's/[][]//g')
