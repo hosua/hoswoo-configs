d577742dc301daa9a73c2b46c84e4f61de0f174e
hosua <hosua.o@gmail.com>
2022-01-25 04:33:42 -0500

    added screenshot folder cleaner

---
+++ Scripts/clean-screenies.sh
@@ -0,0 +1,31 @@
+# A simple script that cleans my screenshots directory when it gets cluttered.
+
+all=$(date | awk '{print $2" "$3" "$7}')
+month=$(echo $all | awk '{print $1}')
+day=$(echo $all | awk '{print $2}')
+year=$(echo $all | awk '{print $3}')
+
+
+echo month: $month
+echo day: $day
+echo year: $year
+DIRNAME="Clutter_$month-$day-$year"
+# Don't try to create the directory if it already exists
+if [ ! -d "$DIRNAME" ]; then
+	echo "Created directory named: $DIRNAME"
+	mkdir "$DIRNAME"
+fi
+
+echo "Moving all images to $DIRNAME"
+
+# Popular image formats I could think off the top of my head
+FTYPES=(".jpg" ".png" ".jpeg" ".gif" ".tif" ".tiff" ".bmp")
+
+for t in ${FTYPES[@]}; do
+	# Take care not to put quotes around any regular expressions. Otherwise you will take
+	# the literal character of that regex (For example, don't quote the *$t* here)
+
+	if [ -f *$t* ]; then 
+		mv *$t* "$DIRNAME"
+	fi
+done
